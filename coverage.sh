713140fde6ed03a17564010fe30be1c168c7d7a7
cgars <christian@stuebeweg50.de>
2017-01-24 18:26:25 +0100

    Test the coverage

--- coverage.sh
+++ coverage.sh
@@ -3,9 +3,13 @@ echo "mode: count" > profile.cov;
 for Dir in $(go list ./...);
 do
   currDir=$(echo $Dir | sed 's/github.com\/G-Node\/gin-repo\///g');
+  echo $currDir
   currCount=$(ls -l $currDir/*.go 2>/dev/null | wc -l);
+  echo $currCount
+  ls -l $currDir
   if [ $currCount -gt 0 ];
   then
+    echo $Dir
     go test -covermode=count -coverprofile=tmp.out $Dir;
     if [ -f tmp.out ];
     then
